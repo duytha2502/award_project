46cccb275c598b363be8e89d1e2f5f58ad7d4d49
duythai2502 <98018388+duytha2502@users.noreply.github.com>
2025-03-27 03:31:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <main>
+            <h1 className='text-violet-300'>Welcome</h1>
+        </main>
+    );
+}
+
+export default App;
