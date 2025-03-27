c60bee4cb97fe9447d68ffa8e72b3c0da1c0879d
duythai2502 <98018388+duytha2502@users.noreply.github.com>
2025-03-28 03:49:09 +0700

    update feature section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -29,10 +30,13 @@ function About() {
                 <p className='font-general text-sm uppercase md:text-[16px]'>
                     Welcome to Zentry
                 </p>
-                <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-                    Disc<b>o</b>ver the world's
-                    <br /> l<b>a</b>rgest shared adventure
-                </div>
+
+                <AnimatedTitle
+                    title="Disc<b>o</b>ver the world's
+            <br /> l<b>a</b>rgest shared adventure"
+                    containerClass='mt-5 !text-black text-center'
+                />
+
                 <div className='about-subtext'>
                     <p>
                         The Game of Games begins-your life, now an epic MMORPG
